60c760dd05ff72764fb17f4c7f09fbd1f5c5dc92
ishitakumbhare2002@gmail.com <ishitakumbhare2002@gmail.com>
2023-04-29 11:11:24 +0530

    Temperature Converter

---
+++ tc.js
@@ -0,0 +1,17 @@
+function fc(temperature) {
+  temperature = parseFloat(temperature);
+  document.getElementById("celsius").value = (temperature - 32) / 1.8;
+  document.getElementById("kelvin").value = ((temperature - 32) / 1.8) + 273.15;
+}
+
+function cc(temperature) {
+  temperature = parseFloat(temperature);
+  document.getElementById("fahrenheit").value = (temperature * 1.8) + 32;
+  document.getElementById("kelvin").value = temperature + 273.15;
+}
+
+function kc(temperature) {
+  temperature = parseFloat(temperature);
+  document.getElementById("fahrenheit").value = ((temperature - 273.15) * 1.8) + 32;
+  document.getElementById("celsius").value = temperature - 273.15;
+}
